0689db3a21e121f7617a209751d7acfad4b46146
Redistributable <a32real@yahoo.co.jp>
2017-06-16 20:36:15 +0900

    Test用のWPF WindowとUserControlをそれぞれ作成

---
+++ EasyCapture/MainClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Markup;
+
+using Redefinable.Applications.EasyCapture.View.XamlTest;
+
+
+namespace Redefinable.Applications.EasyCapture
+{
+    public static class MainClass
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            TestWindow window = new TestWindow();
+            Application app = new Application();
+            app.Run(window);
+        }
+    }
+}
