78e37c99143dfb301a6a0a4ca34056d4543a4a3b
Redistributable <a32real@yahoo.co.jp>
2017-06-20 01:49:34 +0900

    ぎえええええああああああああああああああああああ

---
+++ EasyCapture.View.WinForm/ScreenShotUtility.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace Redefinable.Applications.EasyCapture.View.WinForm
+{
+    public static class ScreenShotUtility
+    {
+        public static Image GetScreenShot()
+        {
+            return GetScreenShot(ScreenShotType.Managed);
+        }
+
+        public static Image GetScreenShot(ScreenShotType type)
+        {
+            Image resultImage = null;
+
+            if (type == ScreenShotType.Managed)
+            {
+                Bitmap bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                    Screen.PrimaryScreen.Bounds.Height);
+                Graphics g = Graphics.FromImage(bmp);
+                g.CopyFromScreen(new Point(0, 0), new Point(0, 0), bmp.Size);
+                g.Dispose();
+
+                resultImage = bmp;
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+
+            return resultImage;
+        }
+    }
+
+    public enum ScreenShotType
+    {
+        Win32API,
+        Managed,
+    }
+}
--- EasyCapture.View/RectCaptureSelectToolWindow.xaml.cs
+++ EasyCapture.View/RectCaptureSelectToolWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Redefinable.Applications.EasyCapture.View
 {
     /// <summary>
@@ -19,21 +20,110 @@ namespace Redefinable.Applications.EasyCapture.View
     /// </summary>
     public partial class RectCaptureSelectToolWindow : Window
     {
+        private double startX;
+        private double startY;
+        private double currentWidth;
+        private double currentHeight;
+
+        
+
         public RectCaptureSelectToolWindow()
         {
             InitializeComponent();
 
+            this.startX = 0;
+            this.startY = 0;
+            this.currentWidth = 0;
+            this.currentHeight = 0;
+
             this.MainGrid.MouseLeftButtonDown += MainGrid_MouseLeftButtonDown;
+            this.MainGrid.MouseMove += MainGrid_MouseMove;
+            this.MainGrid.MouseLeftButtonUp += MainGrid_MouseLeftButtonUp;
         }
 
         private void MainGrid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            var point = e.GetPosition(this);
+            this.startX = point.X;
+            this.startY = point.Y;
+
+            this.SelectRect.Margin = new Thickness(point.X, point.Y, 0, 0);
+            this.SelectRect.Width = 0;
+            this.SelectRect.Height = 0;
+
+            this.SelectRect.Visibility = Visibility.Visible;
+        }
+
+        private void MainGrid_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.SelectRect.Visibility != Visibility.Visible || e.LeftButton == MouseButtonState.Pressed)
+                return;
+
+            var point = e.GetPosition(this.SelectRect);
+            this.currentWidth = point.X;
+            this.currentHeight = point.Y;
+
+            this.SelectRect.Width = point.X;
+            this.SelectRect.Height = point.Y;
+        }
+
+        private void MainGrid_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public RectCaptureSelectToolWindowResult ShowSelectWindow(Window owner)
+        {
+            System.Drawing.Image screenShot = WinForm.ScreenShotUtility.GetScreenShot();
+            BitmapImage bitmapImage = new BitmapImage();
+
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+            {
+                screenShot.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+                ms.Seek(0, System.IO.SeekOrigin.Begin);
+
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = ms;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+            }
+
+
+            this.BackImage.Source = bitmapImage;
+            this.Owner = owner;
+            this.ShowDialog();
+
+            Rect result = default(Rect);
+            
+        }
+    }
+
+    public struct RectCaptureSelectToolWindowResult
+    {
+        private Rect rectangle;
+        private bool cancel;
+
+
+        public Rect Rectangle
+        {
+            get { return this.rectangle; }
+            set { this.rectangle = value; }
+        }
+
+        public bool Cancel
+        {
+            get { return this.cancel; }
+            set { this.cancel = value; }
         }
 
 
-        public static Rect ExecuteSelectRectangle(Application app)
+        public RectCaptureSelectToolWindowResult(Rect rectangle, bool cancel)
         {
-            
+            this.rectangle = rectangle;
+            this.cancel = cancel;
         }
     }
 }
